39e3a6e481414d4a28020bfcb2ce537f7dcf7dbc
hkyoungjeongi <hkyoungjeongi@smilegate.com>
2023-02-13 12:57:34 +0900

    Roll back

--- src/main/java/com/example/signalling2/controller/UserController.java
+++ src/main/java/com/example/signalling2/controller/UserController.java
@@ -13,12 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    @GetMapping("/test/test")
-    public ResponseEntity<String> test() {
-        return ResponseDto.ok("test");
-    }
-    @GetMapping("/new/{userId}")
+    @GetMapping("/{userId}")
     public UserSession getUser(@PathVariable String userId) {
         System.out.println("email received in user controller: " + userId);
         return userService.findById(userId);
