62924555112d67619afde1633eed4cfacecc32fe
hkyoungjeongi <hkyoungjeongi@smilegate.com>
2023-02-15 15:31:50 +0900

    test

--- src/test/java/com/example/signalling2/Signalling2ApplicationTests.java
+++ src/test/java/com/example/signalling2/Signalling2ApplicationTests.java
@@ -4,11 +4,14 @@ import com.example.signalling2.domain.RoomRedis;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.kurento.client.KurentoClient;
+import org.kurento.client.MediaPipeline;
 import org.kurento.client.ServerManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 @DisplayName("Kurento server managing test")
 @SpringBootTest
@@ -20,7 +23,13 @@ class Signalling2ApplicationTests {
     @Test
     void contextLoads() {
         ServerManager serverManager = kurentoClient.getServerManager();
-        System.out.println(serverManager.getSessions());
+        List<MediaPipeline> mediaPipelineList = serverManager.getPipelines();
+        System.out.println(mediaPipelineList);
+        for (MediaPipeline pipeline : mediaPipelineList) {
+            System.out.println(pipeline);
+            System.out.println(pipeline.getId());
+            pipeline.release();
+        }
     }
 
 }
